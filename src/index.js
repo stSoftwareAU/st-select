d96446a48f088388888d19d5c2d33410af954c43
Harry Shaw <harry@stsoftware.com.au>
2019-02-04 16:40:02 +1100

    react changes

--- src/index.js
+++ src/index.js
@@ -10,23 +10,16 @@ class WithCallbacks extends Component {
 
   constructor(props) {
     super(props);
-      console.log("ABC")
-      console.log(props)
     this.state = {
       data: this.props
     };
 
 
     this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleChange = this.handleChange.bind(this);
     this.render = this.render.bind(this);
     this.loadOptions = this.loadOptions.bind(this);
   }
 
-  handleChange() {
-    console.log('asdasd')
-    return;
-  }
 
   handleInputChange(newValue) {
       const inputValue = newValue.replace(/\W/g, '');
@@ -36,19 +29,17 @@ class WithCallbacks extends Component {
       return inputValue;
     }
 
-  loadOptions(inputValue, callback, props){
-    console.log( "XYZ");
-    console.log(this);
+  loadOptions(inputValue, callback){
+    let dataSet = this.props.data.dataset
+    let hostitem = dataSet.host;
+    let classitem = dataSet.class;
+    let nameitem = dataSet.field;
+    console.log(dataSet)
+    console.log(hostitem + " " + classitem + " " + nameitem);
 
-  //this.state.Data
-      console.log(inputValue);
-    // let hostitem = stselects[i].dataset.host;
-    // let classitem = stselects[i].dataset.class;
-    // let nameitem = stselects[i].dataset.name;
-
-    // var url = new URL(hostitem + '/ReST/v8/class/' + classitem)
+     var url = new URL(hostitem + '/ReST/v8/class/' + classitem)
   //  var url = new URL(stselects[i].dataset.host)
-    var url = new URL('https://demo1.jobtrack.com.au/ReST/v8/class/DBFile')
+  //  var url = new URL('https://demo1.jobtrack.com.au/ReST/v8/class/DBFile')
     var params = {q:"name matches '" + inputValue.trim().replace( '\\','\\\\').replace( "'","\\'") +"*'"} // or:
     url.search = new URLSearchParams(params)
 
@@ -65,19 +56,23 @@ class WithCallbacks extends Component {
     .then( results => {
       return results.json();
     }).then( data => {
-      //console.log( data);
+      console.log(data.results[0]);
+      // console.log(data.results[0].name);
       // var tmp= filterColors( inputValue);
       // console.log( tmp);
-      var tmp2=[{
-        value: "a",
-        label: "A"
-      }];
+      var tmp2=[
+        { value: data.results[0]._global_key, label: data.results[0].name},
+        { value: data.results[1]._global_key, label: data.results[1].name},
+        { value: data.results[2]._global_key, label: data.results[2].name},
+        { value: data.results[3]._global_key, label: data.results[3].name},
+        { value: data.results[4]._global_key, label: data.results[4].name},
+
+    ];
       callback( tmp2);
 
     })
   };
-  render(props) {
-    console.log(props)
+  render() {
     return (
       <div>
         <pre>inputValue: "{this.state.inputValue}"</pre>
@@ -93,21 +88,9 @@ class WithCallbacks extends Component {
   }
 }
 
-// var target = Data[0].target
-// console.log(document.getElementsByName(target)[0]);
-//document.getElementById("index")
-//  ReactDOM.render(<WithCallbacks />, document.getElementById("index"));
-//document.getElementsByName('indexTest')[0].prepend('hello')
-//ReactDOM.render(<WithCallbacks />, document.getElementsByName(target)[0].prepend(target));
-
-//ReactDOM.render(<WithCallbacks />, document.getElementById("index"));
-
-// ReactDOM.render(<WithCallbacks />, document.getElementById("index"));
 
 
 let stselects = document.getElementsByClassName('st-select');
 for (var i = 0; i < stselects.length; i++) {
-    // stselects[i];   key={item.id}
-    //console.log( stselects[i]);
-    ReactDOM.render(<WithCallbacks data={stselects[i]}/>, stselects[i]);
+  ReactDOM.render(<WithCallbacks data={stselects[i]}/>, stselects[i]);
 }
